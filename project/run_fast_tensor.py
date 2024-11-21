1cc424d363e4b2145621717811ff5a5691eba404
Aditya Ballaki <77788601+adi-bal@users.noreply.github.com>
2024-11-21 02:32:31 -0500

    GPU optimisation 2

--- project/run_fast_tensor.py
+++ project/run_fast_tensor.py
@@ -29,8 +29,9 @@ class Network(minitorch.Module):
         self.layer3 = Linear(hidden, 1, backend)
 
     def forward(self, x):
-        # TODO: Implement for Task 3.5.
-        raise NotImplementedError("Need to implement for Task 3.5")
+        hidden = self.layer1(x).relu()
+        hidden2 = self.layer2(hidden).relu()
+        return self.layer3(hidden2).sigmoid()
 
 
 class Linear(minitorch.Module):
@@ -43,8 +44,11 @@ class Linear(minitorch.Module):
         self.out_size = out_size
 
     def forward(self, x):
-        # TODO: Implement for Task 3.5.
-        raise NotImplementedError("Need to implement for Task 3.5")
+        batch_size, in_size = x.shape
+        weights = self.weights.value.view(1, in_size, self.out_size)
+        inputs = x.view(batch_size, in_size, 1)
+        output = (weights * inputs).sum(axis=1)
+        return output + self.bias.value
 
 
 class FastTrain:
